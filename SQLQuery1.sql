16599a27bbe09ef9613dc14e4c9e5db7b37ddfde
krusictim <krusic.tim@gmail.com>
2025-10-07 08:40:19 +0200

    Add files via upload
    Pošiljam ti SQL skripto s funkcijami in email naslovi tebe, mene in Urha Kolarja.

---
+++ SQLQuery1.sql
@@ -0,0 +1,26 @@
+ALTER TABLE Ucenec
+ADD COLUMN Email VARCHAR(100),
+ADD COLUMN Funkcija ENUM('uèenec', 'uèitelj', 'skrbnik');
+
+ALTER TABLE Ucitelj
+ADD COLUMN Email VARCHAR(100),
+ADD COLUMN Funkcija ENUM('uèenec', 'uèitelj', 'skrbnik');
+
+INSERT INTO Ucitelj (Ime, Priimek, Email, Funkcija)
+VALUES ('Tim', 'Krušiè', 'tim.krusic.19669@dijak.sc-celje.si', 'skrbnik');
+
+INSERT INTO Ucitelj (Ime, Priimek, Email, Funkcija)
+VALUES ('Marija', 'Èrepinšek', 'marija.crepinsek@dijak.sc-celje.si', 'uèitelj');
+INSERT INTO Ucenec (Ime, Priimek, Email, Funkcija)
+VALUES ('Urh', 'Kolar', 'urh.kolar@dijak.sc-celje.si', 'uèenec');
+UPDATE Ucenec
+SET Email = 'urh.kolar@dijak.sc-celje.si', Funkcija = 'uèenec'
+WHERE Ime = 'Urh' 
+AND Priimek = 'Kolar';
+UPDATE Ucenec
+SET Email = 'tim.krusic@dijak.sc-celje.si', Funkcija = 'uèenec'
+WHERE Ime = 'Tim' 
+AND Priimek='Krušiè';
+UPDATE Ucitelj
+SET Email = 'marija.crepinsek@dijak.sc-celje.si', Funkcija = 'uèitelj'
+WHERE Ime = 'Marija' AND Priimek = 'Èrepinšek';
